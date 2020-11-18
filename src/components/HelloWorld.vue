06d60ee1b7c35cbcbdc68728baf60f96808424cf
Ronan <liruonan@hotmail.com>
2020-11-19 00:24:33 +0800

    ...

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    <pre> 杭州&emsp;富丽小区&emsp;&emsp;&emsp;16,000&emsp;&emsp;&emsp;&emsp;&emsp;2020-09-09 2020-09-09  </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
